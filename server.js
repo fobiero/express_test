4b6a926508b4525f8ae0ec45b27198e5255a8417
feLycks <felix.obiero@student.moringaschool.com>
2021-11-16 09:36:20 +0300

    changed port number

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.delete('/item', (req, res) => {
     res.send(`removed item on port ${PORT}`)
 })
 
-const PORT = 5000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
